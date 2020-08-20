9948a1001c0e80534ae5997649a4ea3e1ce5e598
Carrot-Sword <3375660084@qq.com>
2020-08-20 14:32:00 +0800

    编写了监听器代码
    Signed-off-by: Carrot-Sword <3375660084@qq.com>

--- src/main/java/net/saopj/pvpmanager/Main.java
+++ src/main/java/net/saopj/pvpmanager/Main.java
@@ -3,6 +3,8 @@ package net.saopj.pvpmanager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.saopj.pvpmanager.util.*;
+
 public class Main extends JavaPlugin {
     public static Main ins;
 
@@ -10,6 +12,7 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         reloadConfig();
+        Bukkit.getPluginManager().registerEvents(new Checker(), this);
         Bukkit.getLogger().info("PVP 管理插件已启动.");
     }
 
