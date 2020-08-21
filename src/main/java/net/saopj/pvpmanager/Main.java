45bbd8a0aa9357a2d5f97ca12184b54ee0c58090
Carrot-Sword <3375660084@qq.com>
2020-08-21 14:25:46 +0800

    编写了监听器代码
    Signed-off-by: Carrot-Sword <3375660084@qq.com>

--- src/main/java/net/saopj/pvpmanager/Main.java
+++ src/main/java/net/saopj/pvpmanager/Main.java
@@ -1,9 +1,12 @@
 package net.saopj.pvpmanager;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.saopj.pvpmanager.util.*;
+
+import java.io.File;
 
 public class Main extends JavaPlugin {
     public static Main ins;
@@ -11,6 +14,7 @@ public class Main extends JavaPlugin {
     // 插件启用操作
     public void onEnable() {
         saveDefaultConfig();
+        saveDefaultFile("saves.yml");
         reloadConfig();
         Bukkit.getPluginManager().registerEvents(new Checker(), this);
         Bukkit.getLogger().info("PVP 管理插件已启动.");
@@ -19,6 +23,18 @@ public class Main extends JavaPlugin {
     // 插件禁用操作
     public void onDisable() {
 
+    }
+
+    // 将resources文件夹中的原始文件保存到插件配置文件夹中(如果该文件已经存在，就不会执行操作)
+    public void saveDefaultFile(String fn) {
+        File f = new File(getDataFolder(), fn);
+        if (!f.exists()) {
+            saveResource(fn, false);
+        }
+    }
+
+    public File getSaveFile() {
+        return new File(getDataFolder(), "saves.yml");
     }
 
     public static Main getInstance(){
--- src/main/java/net/saopj/pvpmanager/util/Checker.java
+++ src/main/java/net/saopj/pvpmanager/util/Checker.java
@@ -1,21 +1,52 @@
 package net.saopj.pvpmanager.util;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.EventHandler;
 
+import net.saopj.pvpmanager.Main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Checker implements Listener {
+
+    YamlConfiguration saveConfig;
+    File saveFile;
+
+    public Checker() {
+        this.saveFile = Main.getInstance().getSaveFile();
+        this.saveConfig = YamlConfiguration.loadConfiguration(saveFile);
+    }
 
     // 实体被另外一个实体攻击事件
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent e){
         Damageable victim = (Damageable)e.getEntity();
         if(victim.getType() == EntityType.PLAYER && e.getDamager().getType() == EntityType.PLAYER) {
+            Player damager = (Player) e.getDamager();
             if(e.getFinalDamage() >= victim.getHealth()) {
-
+                Map<String, List<String>> evilPlayers = (Map<String, List<String>>) saveConfig.get("evilPlayer");
+                if (!evilPlayers.containsKey(damager.getName())) {
+                    List<String> evilInfo = new ArrayList<String>();
+                    evilInfo.add("3"); // 这里是罪行等级
+                    evilInfo.add(""); // 这里是解除时间,没写完
+                    evilPlayers.put(damager.getUniqueId().toString(),evilInfo);
+                    saveConfig.set("evilPlayers",evilPlayers);
+                    saveSaveFile();
+                }
             }
         }
+    }
+
+    public void saveSaveFile() {
+        try { saveConfig.save(saveFile); } catch (IOException e){}
     }
 }
